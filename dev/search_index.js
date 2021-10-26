749baa58a76a532c8280068aaab6c8f4c6bd2194
Documenter.jl <documenter@juliadocs.github.io>
2021-10-26 14:24:15 +0000

    build based on 8d9a836

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SchroedingerChess","category":"page"},{"location":"#SchroedingerChess","page":"Home","title":"SchroedingerChess","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SchroedingerChess.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SchroedingerChess]","category":"page"}]
+}
